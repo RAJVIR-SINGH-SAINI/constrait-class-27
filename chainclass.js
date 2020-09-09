d2cf568ba8df80417e4961f99b92997140a6a541
unknown <you@example.com>
2020-09-09 16:07:48 +0530

    constrait class

---
+++ chainclass.js
@@ -0,0 +1,31 @@
+class chain{
+
+constructor(bodyA,bodyB){
+
+var options = {
+bodyA: bodyA,
+bodyB:bodyB,
+stiffness:0.5,
+length:20
+}
+this.chain = Constraint.create(options);
+World.add(world,this.chain);
+}
+
+display(){
+
+    var pointA = this.chain.bodyA.position;
+    var pointB = this.chain.bodyB.position;
+    strokeWeight(5);
+    stroke("red");
+line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+}
+
+
+
+
+
+
+
+}
